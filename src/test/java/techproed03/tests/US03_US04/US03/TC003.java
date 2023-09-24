656b7155676cf2029ccd864b8c179d72703f2a1a
nry46 <nuriyetoprak4663@gmail.com>
2023-09-24 21:07:42 +0200

    Nuriye-24.09.23-v2

--- src/test/java/techproed03/tests/US03_US04/US03/TC003.java
+++ src/test/java/techproed03/tests/US03_US04/US03/TC003.java
@@ -1,35 +1,46 @@
 package techproed03.tests.US03_US04.US03;
 
-import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import techproed03.pages.AlloverPage;
-import techproed03.utilities.ConfigReader;
 import techproed03.utilities.Driver;
 import techproed03.utilities.ReusableMethods;
 
-public class TC001 {
+public class TC003 {
     @Test
-    public void test01() {
-        //Web sitesine gidiniz
+    public void positiveTest() {
+
         //Kullanici olarak giris yapiniz
         MyMethod.userLoginMethod();
         ReusableMethods.bekle(3);
 
         //My Account alanina tiklayiniz
         AlloverPage alloverPage=new AlloverPage();
-        ReusableMethods.scrollEnd();
-        ReusableMethods.bekle(2);
-        alloverPage.myAccountNT.click();
+       ReusableMethods.scrollEnd();
+      ReusableMethods.bekle(2);
+      alloverPage.myAccountNT.click();
 
         //Acilan sayfadan Adresses alanina tiklayiniz
         alloverPage.addressesButtonNT.click();
-        alloverPage.billingAddressAddButtonNT.click();
         ReusableMethods.bekle(2);
+        ReusableMethods.scroll(alloverPage.billingEditYourAddressNT);
+        ReusableMethods.bekle(2);
+        alloverPage.billingEditYourAddressNT.click();
+        //Clear methodu ile dolu alanlari temizleyin
+        ReusableMethods.bekle(2);
+        MyMethod.userBillingClearMethod();
+
         //Zorunlu adres kutucuklarini eksiksiz doldurunuz
-       // MyMethod.billingAddressFormDoldurmaMethodu();
+       MyMethod.userBillingAddressFormDoldurmaMethod();
         ReusableMethods.bekle(2);
-        //Save Adress butonuna tikla
         //''Address changed successfully'' mesajinin alindigini dogrula
+        SoftAssert softAssert=new SoftAssert();
+        softAssert.assertTrue(alloverPage.billingaddressSuccessfullyNT.isDisplayed());
+        Driver.getDriver().close();
+
+
+
+
 
     }
 }
--- src/test/java/techproed03/tests/US12_US13/US_12/US12_Test01.java
+++ src/test/java/techproed03/tests/US12_US13/US_12/US12_Test01.java
@@ -9,57 +9,57 @@ import techproed03.utilities.ConfigReader;
 import techproed03.utilities.Driver;
 
 public class US12_Test01 {
-    @Test
-    public void test01() throws InterruptedException {
-        AlloverPage alloverPage = new AlloverPage();
-
-        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl_ofa"));
-        String alloverUrl = Driver.getDriver().getWindowHandle();
-        Thread.sleep(2000);
-        alloverPage.registerButton_ofa.click();
-        Thread.sleep(2000);
-        alloverPage.becomeVendorButton_ofa.click();
-        Thread.sleep(2000);
-
-        Driver.getDriver().switchTo().newWindow(WindowType.WINDOW);
-        Driver.getDriver().get(ConfigReader.getProperty("FakeMailUrl_ofa"));
-        String FakeMailUrl = Driver.getDriver().getWindowHandle();
-
-        alloverPage.consentFakemail_ofa.click();
-        alloverPage.copyMail_ofa.click();
-
-        Driver.getDriver().switchTo().window(alloverUrl);
-        alloverPage.regEmailBox_ofa.sendKeys(Keys.COMMAND+"v"+Keys.TAB);
-        Thread.sleep(6000);
-
-        Driver.getDriver().switchTo().window(FakeMailUrl);
-        Thread.sleep(4000);
-        Driver.getDriver().navigate().refresh();
-        Thread.sleep(4000);
-
-        alloverPage.mailBox_ofa.click();
-        Thread.sleep(2000);
-        Driver.getDriver().switchTo().frame("iframeMail");
-        String code = alloverPage.code_ofa.getText().replaceAll("[^0-9]", "");
-        System.out.println(code);
-
-        Driver.getDriver().switchTo().window(alloverUrl);
-        Thread.sleep(2000);
-        alloverPage.verCode_ofa.sendKeys(code);
-
-        Driver.getDriver().switchTo().window(FakeMailUrl);
-        alloverPage.fakeBack.click();
-        Thread.sleep(2000);
-        alloverPage.copyPass_ofa.click();
-
-        Driver.getDriver().switchTo().window(alloverUrl);
-        alloverPage.regPasswordBox_ofa.sendKeys(Keys.COMMAND+"v");
-        alloverPage.regPasswordConf_ofa.sendKeys(Keys.COMMAND+"v");
-        alloverPage.regButton_ofa.click();
-
-        Thread.sleep(2000);
-
-
+   // @Test
+//    public void test01() throws InterruptedException {
+//        AlloverPage alloverPage = new AlloverPage();
+//
+//        Driver.getDriver().get(ConfigReader.getProperty("alloverUrl_ofa"));
+//        String alloverUrl = Driver.getDriver().getWindowHandle();
+//        Thread.sleep(2000);
+//        alloverPage.registerButton_ofa.click();
+//        Thread.sleep(2000);
+//        alloverPage.becomeVendorButton_ofa.click();
+//        Thread.sleep(2000);
+//
+//        Driver.getDriver().switchTo().newWindow(WindowType.WINDOW);
+//        Driver.getDriver().get(ConfigReader.getProperty("FakeMailUrl_ofa"));
+//        String FakeMailUrl = Driver.getDriver().getWindowHandle();
+//
+//        alloverPage.consentFakemail_ofa.click();
+//        alloverPage.copyMail_ofa.click();
+//
+//        Driver.getDriver().switchTo().window(alloverUrl);
+//        alloverPage.regEmailBox_ofa.sendKeys(Keys.COMMAND+"v"+Keys.TAB);
+//        Thread.sleep(6000);
+//
+//        Driver.getDriver().switchTo().window(FakeMailUrl);
+//        Thread.sleep(4000);
+//        Driver.getDriver().navigate().refresh();
+//        Thread.sleep(4000);
+//
+//        alloverPage.mailBox_ofa.click();
+//        Thread.sleep(2000);
+//        Driver.getDriver().switchTo().frame("iframeMail");
+//        String code = alloverPage.code_ofa.getText().replaceAll("[^0-9]", "");
+//        System.out.println(code);
+//
+//        Driver.getDriver().switchTo().window(alloverUrl);
+//        Thread.sleep(2000);
+//        alloverPage.verCode_ofa.sendKeys(code);
+//
+//        Driver.getDriver().switchTo().window(FakeMailUrl);
+//        alloverPage.fakeBack.click();
+//        Thread.sleep(2000);
+//        alloverPage.copyPass_ofa.click();
+//
+//        Driver.getDriver().switchTo().window(alloverUrl);
+//        alloverPage.regPasswordBox_ofa.sendKeys(Keys.COMMAND+"v");
+//        alloverPage.regPasswordConf_ofa.sendKeys(Keys.COMMAND+"v");
+//        alloverPage.regButton_ofa.click();
+//
+//        Thread.sleep(2000);
+//
+//
 
 
 
@@ -71,4 +71,4 @@ public class US12_Test01 {
 
 
     }
-}
+//}
